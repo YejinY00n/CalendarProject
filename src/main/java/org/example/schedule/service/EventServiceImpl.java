0c162f9016bb4b9149fb12a49492c0c7865bb491
YejinY00n <yoon17710@gmail.com>
2025-03-26 01:56:02 +0900

    Feat: 일정 업데이트 - 비밀번호 검증 추가

--- src/main/java/org/example/schedule/service/EventServiceImpl.java
+++ src/main/java/org/example/schedule/service/EventServiceImpl.java
@@ -35,7 +35,7 @@ public class EventServiceImpl implements EventService {
       LocalDateTime startDate,
       LocalDateTime endDate) {
 
-    List<Event> results = null;
+    List<Event> results;
 
     if (isValidDateRange(startDate, endDate)) {
       // 작성자의 기간 내의 일정 조회
@@ -63,6 +63,51 @@ public class EventServiceImpl implements EventService {
         .collect(Collectors.toList());
   }
 
+  // Id를 통해 단건 일정을 조회하는 메소드
+  @Override
+  public EventResponseDTO findEventById(Long id) {
+    return new EventResponseDTO(eventRepository.findEventById(id));
+  }
+
+  // 일정을 업데이트하는 메소드
+  @Override
+  @Transactional
+  public EventResponseDTO updateEvent(Long id, EventRequestDTO requestDTO) {
+    // 일정과 작성자 공백 or null 검증
+    if ((requestDTO.getOwner() == null || requestDTO.getOwner().isEmpty()) && (
+        requestDTO.getTask() == null || requestDTO.getTask().isEmpty())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "일정 혹은 작성자를 입력해주세요.");
+    }
+
+    // 비밀번호 검증
+    if (!isValidPassword(id, requestDTO.getPassword())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다.");
+    }
+
+    // 일정 수정
+    int updatedRow = eventRepository.updateEvent(id, requestDTO.getTask(), requestDTO.getOwner());
+
+    // 수정된 일정이 없을 경우
+    if (updatedRow == 0) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+          "일정이 존재하지 않습니다. (ID: " + id + " )");
+    }
+
+    // 데이터가 정상적으로 수정되었는지 조회 후 반환
+    return new EventResponseDTO(eventRepository.findEventById(id));
+  }
+
+  // 일정을 삭제하는 메소드
+  @Override
+  public void deleteEvent(Long id, String password) {
+    int deletedRow = eventRepository.deleteEvent(id, password);
+
+    // 삭제 대상 일정이 없을 경우
+    if (deletedRow == 0) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "일정이 존재하지 않습니다. (ID: " + id + " )");
+    }
+  }
+
   // 유효한 기간인지 검증하는 메소드
   private boolean isValidDateRange(LocalDateTime startTime, LocalDateTime endTime) {
     // 조회 시작일, 끝일 중 하나라도 없다면
@@ -75,39 +120,8 @@ public class EventServiceImpl implements EventService {
     }
   }
 
-  // Id를 통해 단건 일정을 조회하는 메소드
-  @Override
-  public EventResponseDTO findEventById(Long id) {
-    return new EventResponseDTO(eventRepository.findEventById(id));
-  }
-
-  // TODO: 값 검증 로직 추가
-  // TODO: 비밀번호 검증 로직 추가
-  // TODO: Update 리퀘스트 DTO 새로 생성 필요 (생성, 수정날짜, id..) 혹은 DTO 생성자
-  // 일정을 업데이트하는 메소드
-  @Override
-  @Transactional
-  public EventResponseDTO updateEvent(Long id, EventRequestDTO requestDTO) {
-    int updatedRow = eventRepository.updateEvent(id, requestDTO.getTask(), requestDTO.getOwner());
-
-    // 수정된 일정이 없을 경우
-    if (updatedRow == 0) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-          "일정이 존재하지 않습니다. (ID: " + id + " )");
-    }
-
-    // 데이터가 정상적으로 수정되었는 지 조회 후 반환
-    return new EventResponseDTO(eventRepository.findEventById(id));
-  }
-  
-  // 일정을 삭제하는 메소드
-  @Override
-  public void deleteEvent(Long id, String password) {
-    int deletedRow = eventRepository.deleteEvent(id, password);
-
-    // 삭제 대상 일정이 없을 경우
-    if (deletedRow == 0) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "일정이 존재하지 않습니다. (ID: " + id + " )");
-    }
+  // 선택한 일정의 비밀번호와 일치 여부 검증
+  private boolean isValidPassword(Long id, String password) {
+    return eventRepository.findEventById(id).getPassword().equals(password);
   }
 }
